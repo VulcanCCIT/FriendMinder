0f144e66938fd7dd8bba98f4315119a457209ff5
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-09-05 18:51:58 -0500

    Initial Commit

---
+++ FriendMinder/FriendMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendMinderApp.swift
+//  FriendMinder
+//
+//  Created by Chuck Condron on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
